2534c8f200b8898735d7675990220446f0735991
rodrigoadati <rodrigo_adati@hotmail.com>
2017-07-13 21:32:56 +1200

    Visual Studio Project
    Added visual studio project

---
+++ COMP743-WebDevelopment/COMP743-WebDevelopment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP743_WebDevelopment.MvcApplication" Language="C#" %>
---
+++ COMP743-WebDevelopment/COMP743-WebDevelopment/Infrastructure/Connections.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP743_WebDevelopment.Infrastructure
+{
+    public class Connections
+    {
+        public static string connection = @"Data Source=.\SQLEXPRESS;Initial Catalog=ProductStore;Integrated Security=True";
+    }
+}
